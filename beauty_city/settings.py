d40077106f1966f386a420db8475ac0df5272501
PaSeRouS <paser96@mail.ru>
2022-12-14 19:02:09 +0300

    add index page

--- beauty_city/settings.py
+++ beauty_city/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bc_site',
     'salons',
-    'customers'
+    'customers',
 ]
 
 MIDDLEWARE = [
--- beauty_city/urls.py
+++ beauty_city/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.shortcuts import render
+from django.urls import include, path
+
+from bc_site import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='start_page'),
+    # path('lk/', views.notes, name='lk')
 ]
